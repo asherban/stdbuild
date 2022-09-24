af59e72ee3050a401187b2c4b9db75cf599818fb
asherban <andrei.sherban@gmail.com>
2022-09-24 17:18:22 +0300

    Basic installation and build support

--- cplus.cpp
+++ cplus.cpp
@@ -2,9 +2,34 @@
 
 #include <iostream>
 #include <string>
+#include <filesystem>
 #include <termcolor/termcolor.hpp>
 
-void installPackage(const std::string& packageName) {}
+void installPackage(const std::string& packageName) {
+    YAML::Node config = YAML::LoadFile("packages/" + packageName + ".yaml");
+
+    auto path = std::filesystem::current_path();
+    auto packageBaseDir = path / "vendor";
+    auto packagePath = packageBaseDir / packageName;
+
+    if (std::filesystem::exists(packagePath)) {
+        return;
+    }
+
+    std::filesystem::create_directories(packageBaseDir);
+    std::filesystem::current_path(packageBaseDir);
+
+    std::string gitCommand = "git clone --depth=1 --branch=" + config["version"].as<std::string>() + " " + config["repo"].as<std::string>() + " " + packageName;
+    std::system(gitCommand.c_str());
+
+    std::filesystem::current_path(packagePath);
+
+    if (config["build"]) {
+        std::system(config["build"].as<std::string>().c_str());
+    }
+
+    std::filesystem::current_path(path);
+}
 
 const std::string compilerOptions(const std::string& packageName) {
     YAML::Node config = YAML::LoadFile("packages/" + packageName + ".yaml");
@@ -21,20 +46,20 @@ const std::string compilerOptions(const std::string& packageName) {
     for (auto it : config["link"]) {
         oss << "-l" << it << " ";
     }
+
     return oss.str();
 }
 
-void executeCompiler(const YAML::Node & config, const std::string& file) {
-
+void executeCompiler(const YAML::Node& config, const std::string& file) {
     std::stringstream oss;
-    oss << "g++ " << file << " ";
+    oss << "g++ --std=c++17 " << file << " ";
     for (auto it : config["dependencies"]) {
         std::string packageName = it.as<std::string>();
         std::string opts = compilerOptions(packageName);
         oss << opts << " ";
     }
     oss << "--static ";
-    oss << "-o " << config["name"];
+    oss << "-o bin/" << config["name"];
     std::string cmd = oss.str();
 
     std::system(cmd.c_str());
@@ -47,13 +72,15 @@ int main(int /*argc*/, char** /*argv*/) {
     std::cout << std::endl;
 
     YAML::Node config = YAML::LoadFile("cplus.yaml");
-    std::cout << config["name"] << std::endl;
 
-    std::list<std::string> files = { "cplus.cpp" };
-    for (auto & file : files) {
-         executeCompiler(config, file);
+    for (auto pkg : config["dependencies"]) {
+        installPackage(pkg.as<std::string>());
     }
 
+    std::list<std::string> files = {"cplus.cpp"};
+    for (auto& file : files) {
+        executeCompiler(config, file);
+    }
 
     return 0;
 }
